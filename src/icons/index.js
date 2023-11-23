9f03d8a8ccb1be816e4b46b426c1f4116c10f6eb
AbhishekBhure <abhiab7411@gmail.com>
2023-11-23 22:33:48 +0530

    index.js

---
+++ src/icons/index.js
@@ -0,0 +1,7 @@
+import add from "./add.png";
+import trash from "./trash.png";
+import edit from "./edit.png";
+import save from "./save.png";
+import add2 from "./add2.png";
+
+export { add, add2, trash, edit, save };
