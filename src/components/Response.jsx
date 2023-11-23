61ca642a910953e67830d5b071eafde599b17d90
AbhishekBhure <abhiab7411@gmail.com>
2023-11-23 22:32:13 +0530

    modified Response.jsx

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { add, trash, edit, save } from "../icons";
 
 const Response = () => {
   const [todos, setTodos] = useState([]);
@@ -6,7 +7,9 @@ const Response = () => {
   const [showInput, setShowInput] = useState(false);
   const [isInputEmpty, setIsInputEmpty] = useState(true);
   const [editIndex, setEditIndex] = useState(null);
+  const [isTodoLimitReached, setIsTodoLimitReached] = useState(false);
 
+  var maxCharater = 150;
   const handleAddTodo = () => {
     if (editIndex !== null) {
       // Edit existing todo
@@ -15,9 +18,13 @@ const Response = () => {
       setTodos(updatedTodos);
       setEditIndex(null);
     } else {
-      // Add new todo
-      if (newTodo.trim() !== "") {
-        setTodos([newTodo, ...todos]);
+      // Add new todo if the limit is not reached
+      if (todos.length < 10 && newTodo.trim() !== "") {
+        setTodos([...todos, newTodo]);
+      } else {
+        // Update state to indicate that the todo limit is reached
+        setIsTodoLimitReached(true);
+        return; // Exit the function without further execution
       }
     }
 
@@ -41,38 +48,23 @@ const Response = () => {
   const handleEdit = (index) => {
     setEditIndex(index);
     setNewTodo(todos[index]);
-    // setShowInput(true);
   };
 
   const handleDelete = (index) => {
     const updatedTodos = [...todos];
     updatedTodos.splice(index, 1);
     setTodos(updatedTodos);
+
+    // Check if the todo limit was reached and update the state
+    if (isTodoLimitReached && updatedTodos.length < 10) {
+      setIsTodoLimitReached(false);
+    }
   };
 
   return (
-    <div className="todo-container">
-      <div className="todo-header">
-        <h1>ToDo List</h1>
-      </div>
-      <div className="todo-body">
-        <div className="todo-input">
-          {showInput && (
-            <div>
-              <input
-                type="text"
-                value={newTodo}
-                onChange={handleInputChange}
-                placeholder="Enter a new task"
-              />
-              <button onClick={handleAddTodo} disabled={isInputEmpty}>
-                {editIndex !== null ? "Edit" : "Add"}
-              </button>
-              <button onClick={handleCancel}>Cancel</button>
-            </div>
-          )}
-        </div>
-        <ul className="todo-list">
+    <div className="response-container">
+      <div className="response-body">
+        <ul className="response-list">
           {todos.map((todo, index) => (
             <li key={index}>
               {editIndex === index ? (
@@ -81,23 +73,113 @@ const Response = () => {
                     type="text"
                     value={newTodo}
                     onChange={handleInputChange}
+                    maxLength={maxCharater}
                   />
-                  <button onClick={handleAddTodo} disabled={isInputEmpty}>
-                    {editIndex !== null ? "Edit" : "Add"}
+                  <span>
+                    {newTodo.length} / {maxCharater}
+                  </span>
+                  <button
+                    className={
+                      isInputEmpty
+                        ? "edit-Response-btn grey"
+                        : "edit-Response-btn"
+                    }
+                    onClick={handleAddTodo}
+                    disabled={isInputEmpty || isTodoLimitReached}
+                  >
+                    {editIndex !== null ? (
+                      <span>
+                        {" "}
+                        <img src={save} alt="save" /> Save
+                      </span>
+                    ) : (
+                      <span>
+                        {" "}
+                        <img src={add} alt="addIcon" /> Add
+                      </span>
+                    )}
                   </button>
-                  <button onClick={handleCancel}>Cancel</button>
+                  <button
+                    className="add-delete-Response-btn"
+                    onClick={handleCancel}
+                  >
+                    {" "}
+                    Cancel
+                  </button>
                 </div>
               ) : (
                 <p>
                   {todo}
-                  <button onClick={() => handleEdit(index)}>edit</button>
-                  <button onClick={() => handleDelete(index)}>delete</button>
+                  <span>
+                    {todo.length}/{maxCharater}
+                  </span>
+                  <button
+                    className="edit-Response-btn"
+                    onClick={() => handleEdit(index)}
+                  >
+                    <span>
+                      <img src={edit} alt="editIcon" />
+                    </span>
+                    Edit
+                  </button>
+                  <button
+                    className="add-delete-Response-btn"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <span>
+                      <img src={trash} alt="trashIcon" />
+                    </span>
+                    Delete
+                  </button>
                 </p>
               )}
             </li>
           ))}
         </ul>
-        <button onClick={() => setShowInput(true)}>Add Todo</button>
+        <div className="response-input">
+          {showInput && (
+            <div>
+              <input
+                type="text"
+                value={newTodo}
+                onChange={handleInputChange}
+                placeholder="Enter a new task"
+                maxLength={maxCharater}
+              />
+
+              <button
+                className={
+                  isInputEmpty ? "edit-Response-btn grey" : "edit-Response-btn"
+                }
+                onClick={handleAddTodo}
+                disabled={isInputEmpty || isTodoLimitReached}
+              >
+                {editIndex !== null ? (
+                  <span>
+                    {" "}
+                    <img src={edit} alt="edit" /> Edit
+                  </span>
+                ) : (
+                  <span>
+                    {" "}
+                    <img src={add} alt="addIcon2" /> Add
+                  </span>
+                )}
+              </button>
+              <button onClick={handleCancel}>Cancel</button>
+            </div>
+          )}
+        </div>
+        <button
+          className="add-delete-Response-btn "
+          onClick={() => setShowInput(true)}
+          disabled={isTodoLimitReached || todos.length >= 10}
+        >
+          <span>
+            <img src={add} alt="addIcon" />
+          </span>
+          Add a new quick response
+        </button>
       </div>
     </div>
   );
